5bf0cc24fbcdeec425b2a996ecf5a8a73c5f9d50
ashu <ashishaundhe7877@gmail.com>
2023-02-06 12:30:39 +0530

    javascript

---
+++ console.js
@@ -0,0 +1,25 @@
+//string methods
+
+let a= "hadapsar";
+let b = a.charCodeAt(1);
+console.log(b);
+
+let c =a.charAt(2)
+console.log(c);
+
+let d= a.repeat(10);
+console.log(d);
+
+let e=a.toLocaleLowerCase();
+console.log(e);
+
+let f= a.toLocaleUpperCase();
+console.log(f);
+
+let g = a.length
+console.log(h);
+
+let i = a.replace("hadapsar","Pune")
+console.log(i);
+
+let j=a.startsWith("H");
