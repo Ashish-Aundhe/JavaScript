c6f59bc161cdda17f6614f2bf1b3ddc5942debd8
Ashish-Aundhe <123938575+Ashish-Aundhe@users.noreply.github.com>
2023-04-09 23:51:46 +0530

    Add files via upload

---
+++ JS/fun2.js
@@ -0,0 +1,16 @@
+console.log("Start");
+var a=10;
+console.log(a);
+function x (){
+	var b =30;
+	var c= 20;
+	function y (){
+	var d ="400"
+	console.log(b);
+	console.log(c);
+	consloe.log(d);
+	}
+	return y
+}
+x()()
+console.log("end");
